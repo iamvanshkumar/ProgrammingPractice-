1065f2bd202cc20cd3cf4ddf8eb893dbfa97cea6
Vansh Kumar <vanshkumar.mail@gmail.com>
2023-05-30 19:14:32 +0530

    Create C Program to Find the Largest Number Among Three Numbers.c

---
+++ C Programming/C Program to Find the Largest Number Among Three Numbers.c
@@ -0,0 +1,22 @@
+//C Program to Find the Largest Number Among Three Numbers
+
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+	int a,b,c;
+
+	printf("Enter three numbers : \n");
+	scanf("%d%d%d",&a,&b,&c);
+	
+	if(a>b && a>c){
+		printf("%d is largest",a);		
+	}
+	else if(b>a && b>c){
+		printf("%d is largest",b);		
+	}
+	else{
+		printf("%d is largest",c);
+	}
+	
+}
