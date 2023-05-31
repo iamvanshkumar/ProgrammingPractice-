63c493e22306dfa130cea40236a03024f2e52dad
Vansh Kumar <vanshkumar.mail@gmail.com>
2023-05-31 11:51:14 +0530

    Update Check leap Year.c

--- C Programming/Check leap Year.c
+++ C Programming/Check leap Year.c
@@ -1,1 +1,20 @@
 //check leap year 
+
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+	int year;
+	printf("Enter Year : ");
+	scanf("%d",&year);
+	
+	if(year%400==0 || year%4 ==0){
+		printf("Leap Year");
+	}
+	else if (year%100==0){
+		printf("Not a Leap Year");
+	}
+	else{
+		printf("Not a Leap Year");
+	}
+}
